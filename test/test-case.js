5002ae2be13f4055bece2ec38b7b3b2347b3e94c
Greg Bowler <greg.bowler@g105b.com>
2015-04-20 17:17:47 +0100

    Test properties are exported

--- test/test-case.js
+++ test/test-case.js
@@ -2,18 +2,20 @@
 var test = require("tape");
 var ns = require("../src/ns");
 
-// test("timing test", function (t) {
-//     t.plan(2);
-
-//     t.equal(typeof Date.now, "function");
-//     var start = Date.now();
-
-//     setTimeout(function () {
-//         t.equal(Date.now() - start, 100);
-//     }, 100);
-// });
-
 test("Load ns", function(t) {
-	t.plan(1);
-	t.equal(105, 7+7*7+7*7);
+	var
+		prop,
+		count = 0,
+
+		expectedProperties = ["ns", "go"],
+	$$;
+
+	for(prop in ns) {
+		t.ok(expectedProperties.indexOf(prop) >= 0, "ns exposes properties");
+		count++;
+	}
+
+	t.equal(expectedProperties.length, count);
+
+	t.end();
 });
