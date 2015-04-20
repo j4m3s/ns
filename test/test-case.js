5bb1b35a7557933330afab49813105ea3e2b125d
Greg Bowler <greg.bowler@g105b.com>
2015-04-20 17:05:30 +0100

    Require ns for tests

--- test/test-case.js
+++ test/test-case.js
@@ -1,12 +1,19 @@
+// console.log(require);
 var test = require("tape");
+var ns = require("../src/ns");
 
-test("timing test", function (t) {
-    t.plan(2);
+// test("timing test", function (t) {
+//     t.plan(2);
 
-    t.equal(typeof Date.now, "function");
-    var start = Date.now();
+//     t.equal(typeof Date.now, "function");
+//     var start = Date.now();
 
-    setTimeout(function () {
-        t.equal(Date.now() - start, 100);
-    }, 100);
+//     setTimeout(function () {
+//         t.equal(Date.now() - start, 100);
+//     }, 100);
+// });
+
+test("Load ns", function(t) {
+	t.plan(1);
+	t.equal(105, 7+7*7+7*7);
 });
